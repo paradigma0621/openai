c6c5dc4f51f3d8e48d22dd75ed8f638cec02e48f
Lucas <paradigma0621@gmail.com>
2025-03-01 15:17:51 -0300

    Add endpoint for Similarity Embedding Finder

--- src/main/java/com/paradigma0621/openai/controller/EmbeddingController.java
+++ src/main/java/com/paradigma0621/openai/controller/EmbeddingController.java
@@ -4,6 +4,7 @@ import com.paradigma0621.openai.service.EmbeddingService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -18,5 +19,11 @@ public class EmbeddingController {
     public String embed(@RequestBody String text) { // 'text' can be any string (a single word or a sentence)
         float[] response = service.embed(text);
         return Arrays.toString(response);
+    }
+
+    @GetMapping("/similarityFinder")
+    public double findSimilarity(@RequestParam String text1, @RequestParam String text2) { // 'text1' and 'text2' can be
+                                                                            // any string (a single word or a sentence)
+        return service.findSimilarity(text1, text2);
     }
 }
--- src/main/java/com/paradigma0621/openai/service/EmbeddingService.java
+++ src/main/java/com/paradigma0621/openai/service/EmbeddingService.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class EmbeddingService {
@@ -12,5 +14,31 @@ public class EmbeddingService {
 
     public float[] embed(String text) {
         return embeddingModel.embed(text);
+    }
+
+    public double findSimilarity(String text1, String text2) {
+        var response = embeddingModel.embed(List.of(text1, text2));
+        return cosineSimilarity(response.get(0), response.get(1));
+    }
+
+    private double cosineSimilarity(float[] vectorA, float[] vectorB) {
+        if (vectorA.length != vectorB.length) {
+            throw new IllegalArgumentException("Vectors must be of the same length");
+        }
+
+        // Initialize variables for dot product and magnitudes
+        var dotProduct = 0.0;
+        var magnitudeA = 0.0;
+        var magnitudeB = 0.0;
+
+        // Calculate dot product and magnitudes
+        for (var i = 0; i < vectorA.length; i++) {
+            dotProduct += vectorA[i] * vectorB[i];
+            magnitudeA += vectorA[i] * vectorA[i];
+            magnitudeB += vectorB[i] * vectorB[i];
+        }
+
+        // Calculate and return cosine similarity
+        return dotProduct / (Math.sqrt(magnitudeA) * Math.sqrt(magnitudeB));
     }
 }
