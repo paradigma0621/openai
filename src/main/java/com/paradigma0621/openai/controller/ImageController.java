9f0706330d3be9f44ad0e6fdb4f30d22b699a7dd
Lucas <paradigma0621@gmail.com>
2025-03-04 15:15:58 -0300

    Add image analyser endpoint

--- src/main/java/com/paradigma0621/openai/controller/ImageController.java
+++ src/main/java/com/paradigma0621/openai/controller/ImageController.java
@@ -4,17 +4,50 @@ import com.paradigma0621.openai.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.*;
 
 @RequiredArgsConstructor
 @RestController
 public class ImageController {
+
+	private static final String UPLOAD_DIR = "/home/paradigma0621/Documents/images/";
 
 	private final ImageService service;
 
 	@PostMapping("/imageGenerator")
 	public String imageGenerator(@RequestBody String prompt) {
 		return service.generateImage(prompt);
+	}
+
+	@PostMapping("/imageAnalyzer")
+	public String uploadImage(String prompt,  // Given question about the image to be passed by parameter
+							  @RequestParam("file") MultipartFile file) {
+		if (file.isEmpty()) {
+			return "Please select a file to upload";
+		}
+
+		try {
+			// Ensure the directory exists
+			Path uploadDir = Paths.get(UPLOAD_DIR);
+			if (Files.notExists(uploadDir)) {
+				Files.createDirectories(uploadDir); // Create the directory if it doesn't exist
+			}
+
+			// Save the uploaded file to the specified directory
+			Path path = uploadDir.resolve(file.getOriginalFilename());
+			Files.write(path, file.getBytes(), StandardOpenOption.CREATE);
+
+			// Generate explanation and add to the model
+			return service.explainImage(prompt,path.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Failed to upload file. " + e.getMessage();
+		}
 	}
 
 }
--- src/main/java/com/paradigma0621/openai/service/ImageService.java
+++ src/main/java/com/paradigma0621/openai/service/ImageService.java
@@ -1,16 +1,21 @@
 package com.paradigma0621.openai.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 
 @RequiredArgsConstructor
 @Service
 public class ImageService {
 
     private final OpenAiImageModel openaiImageModel;
+
+    private final ChatClient chatClient;
 
     public String generateImage(String prompt) {
         var response = openaiImageModel.call(new ImagePrompt(prompt,
@@ -22,5 +27,12 @@ public class ImageService {
                         .build()));
 
         return response.getResult().getOutput().getUrl();
+    }
+
+
+    public String explainImage(String prompt, String path) {
+        return chatClient.prompt()
+                .user(u -> u.text(prompt).media(MimeTypeUtils.IMAGE_JPEG, new FileSystemResource(path))).call()
+                .content();
     }
 }
